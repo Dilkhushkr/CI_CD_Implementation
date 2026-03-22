f06c9da7b2f36a9cb5350a6e5f8853f8c14cee6e
Dilkhushkr <128610575+Dilkhushkr@users.noreply.github.com>
2026-03-23 00:07:35 +0530

    new_changes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,21 @@ const app = express();
 
 app.use(express.json());
 
+let todos = []
+
 app.get("/", (req, res) => {
   res.send("API Running 🚀");
+});
+
+app.get("/todos",(req, res) => {
+    res.json(todos)
+})
+
+
+app.post("/todos", (req, res) => {
+  const todo = { id: Date.now(), text: req.body.text };
+  todos.push(todo);
+  res.status(201).json(todo);
 });
 
 
